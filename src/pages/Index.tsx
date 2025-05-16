a3870ce10876a28d59e420397b86339edb663791
VINAYSAI31 <vinaysai1133@gmail.com>
2025-05-16 23:57:31 +0530

    updated contact

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -6,8 +6,6 @@ import TrustedBy from '../components/TrustedBy';
 import Testimonials from '../components/Testimonials';
 import ContactSection from '../components/ContactSection';
 import Portfolio from './Portfolio';
-import Contact from '../components/ContactUs.jsx';
-
 const Index = () => {
   return (
     <div className="min-h-screen bg-lumo-900">
@@ -17,8 +15,7 @@ const Index = () => {
       <TrustedBy />
       <Portfolio/>
       <Testimonials />
-      <Contact/>
-      {/* <ContactSection /> */}
+      <ContactSection />
     </div>
   );
 };
