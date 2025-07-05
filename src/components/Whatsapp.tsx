00d0f5a47812be73bf7c1d28de83ee7dfb6501e1
vamsi1465 <vamsikrishna2536@gmail.com>
2025-07-06 01:39:21 +0530

    Updated latest project files

--- src/components/Whatsapp.tsx
+++ src/components/Whatsapp.tsx
@@ -4,11 +4,11 @@ const Whatsapp: React.FC = () => {
   return (
     <a
       href="https://wa.me/+918074328526" 
-      className="fixed bottom-6 right-6 bg-green-500 w-16 h-16 rounded-full flex items-center justify-center shadow-lg hover:scale-110 transition-transform duration-300 animate-slow-bounce  z-50 group"
+      className="fixed bottom-6 left-6 bg-green-500 w-16 h-16 rounded-full flex items-center justify-center shadow-lg hover:scale-110 transition-transform duration-300 animate-slow-bounce z-50 group"
       target="_blank"
       rel="noopener noreferrer"
     >
-      <span className="absolute right-20 bg-gray-800 text-white p-2 rounded-md text-xs opacity-0 transition-opacity duration-300 group-hover:opacity-100 whitespace-nowrap">
+      <span className="absolute left-20 bg-gray-800 text-white p-2 rounded-md text-xs opacity-0 transition-opacity duration-300 group-hover:opacity-100 whitespace-nowrap">
         Chat with us!
       </span>
       <i className="fab fa-whatsapp text-white text-3xl relative z-10" />
